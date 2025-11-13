936b70e67bf3c68dc506c673c0eed1189a81d6f5
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-13 16:00:30 +0600

    added user profile router, controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { loginUser, myProfile } from '../controllers/user.js'
+import { loginUser, myProfile, userProfile } from '../controllers/user.js'
 import { isAuth } from '../middleware/isAuth.js';
 
 const router = express.Router()
@@ -8,6 +8,6 @@ const router = express.Router()
 
 router.post('/login',loginUser);
 router.get('/me',isAuth,myProfile);
-
+router.get('/user/:id',userProfile);
 
 export default router
