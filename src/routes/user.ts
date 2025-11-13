afd7a58d2ee9f912e53503baa0020a04c1017a40
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-13 13:10:35 +0600

    modified root get route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,15 +1,9 @@
-import express, { type Request, type Response } from 'express'
+import express from 'express'
 import { loginUser } from '../controllers/user.js'
 
 const router = express.Router()
 
-router.get('/',(req:Request,res:Response)=>{
-    res.status(200).json({
-        message:'Server is live successfully',
-        data:'',
-        success:true
-    })
-})
+
 
 router.post('/login',loginUser)
 export default router
