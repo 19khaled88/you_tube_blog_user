342c457ad671446d80fd8f5e690c13e6ebb90d81
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-13 15:42:59 +0600

    added custom trycatch block, test my login auth

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,34 +1,39 @@
 import type { Request, Response } from "express";
 import User from "../model/User.js";
 import jwt from 'jsonwebtoken'
+import TryCatch from "../utils/TryCatch.js";
+import type { AuthenticationRequest } from "../middleware/isAuth.js";
 
 
-export const loginUser = async(req:Request,res:Response)=>{
-    try {
-        const {email,name,image} = req.body;
+export const loginUser = TryCatch(async (req, res) => {
+    const { email, name, image } = req.body;
 
-        let user = await User.findOne({email});
+    let user = await User.findOne({ email });
 
-        if(!user){
-            user = await User.create({
-                name,
-                email,
-                image,
-            });
-        }
-
-        const token = jwt.sign({user},process.env.JWT_SEC as string,{
-            expiresIn:"5d",
+    if (!user) {
+        user = await User.create({
+            name,
+            email,
+            image,
         });
-
-        res.status(200).json({
-            message:'User created successfully',
-            token,
-            user
-        })
-    } catch (error:any) {
-        res.status(500).json({
-            message:error.message
-        })
     }
-}
+
+    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+        expiresIn: "5d",
+    });
+
+    res.status(200).json({
+        message: 'User created successfully',
+        token,
+        user
+    })
+});
+
+
+
+
+export const myProfile = TryCatch(async (req:AuthenticationRequest, res) => {
+    const user = req.user;
+
+    res.json(user)
+})
