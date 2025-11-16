cdb8c5d4529f8ebba4d76fc86f9e20cc6e1ecfdc
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-16 17:37:02 +0600

    added file or image upload router, added mutler, and multer config

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import { loginUser, myProfile, updateUser, userProfile } from '../controllers/user.js'
+import { loginUser, myProfile, updateProfilePicture, updateUser, userProfile } from '../controllers/user.js'
 import { isAuth } from '../middleware/isAuth.js';
+import upload from '../middleware/multer.js';
 
 const router = express.Router()
 
@@ -9,6 +10,7 @@ const router = express.Router()
 router.post('/login',loginUser);
 router.get('/me',isAuth,myProfile);
 router.get('/user/:id',userProfile);
-router.post('/user/update',isAuth,updateUser)
+router.post('/user/update',isAuth,updateUser);
+router.post('/user/update/profile-picture',isAuth,upload,updateProfilePicture);
 
 export default router
