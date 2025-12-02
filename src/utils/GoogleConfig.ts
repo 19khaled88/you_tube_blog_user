318d97a3daa4d624c8b85e12d1b97e43dba0f8e9
19khaled88 <kkhaled88hasan@gmail.com>
2025-12-03 01:34:51 +0600

    user commit

--- src/utils/GoogleConfig.ts
+++ src/utils/GoogleConfig.ts
@@ -13,5 +13,8 @@ export const oauth2client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
     // "postmessage"
-    "https://you-tube-blog-web.vercel.app/auth/callback"
+    // "https://you-tube-blog-web.vercel.app/auth/callback",
+    // 'http://localhost:3005/api/auth/callback/google'
+    'https://you-tube-blog-web.vercel.app'
+
 );
